8f80e0d6be22192d38a1541256d4315437ce80ed
erickresend <erick.resend@gmail.com>
2023-12-26 15:01:57 -0300

    Aula 11 Angular

--- curso-angular-loiane-groner-yt/data-binding/src/app/data-binding/data-binding.component.ts
+++ curso-angular-loiane-groner-yt/data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,11 +11,31 @@ export class DataBindingComponent {
   cursoAngular: boolean = true;
   urlImagem = "https://ihg.scene7.com/is/image/ihg/ihg-trip-ideas-lp-home-featured-nyc-400x200"
 
+  valorAtual = '';
+  valorSalvo: any = '';
+  isMouseOver = false;
+
   getValor(){
     return 1;
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert("Botão clicado!")
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+
+  salvarValor(valor: any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 }
